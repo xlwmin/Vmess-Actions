f07c40441a734e46f6fb7030c75bef47697da36a
ldir92664 <ldir92664@163.com>
2021-09-01 17:19:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ def main(event, context):
     utils.initLog('log.txt')
     utils.clearLog()
     savePoint(
-        'https://sspool.herokuapp.com/clash/proxies?nc=CN,HK,TW&speed=10,50&type=vmess', 'vmess.txt')
+        'https://sspool.herokuapp.com/clash/proxies?nc=CN,HK,TW&speed=30&type=vmess', 'vmess.txt')
 
     savePoint(
-        'https://sspool.herokuapp.com/clash/proxies?nc=CN,HK,TW&speed=10,50&type=ss', 'ss.txt')
+        'https://sspool.herokuapp.com/clash/proxies?nc=CN,HK,TW&speed=30&type=ss', 'ss.txt')
 
     savePoint(
-        'https://sspool.herokuapp.com/clash/proxies?nc=CN,HK,TW&speed=10,50&type=ssr', 'ssr.txt')
+        'https://sspool.herokuapp.com/clash/proxies?nc=CN,HK,TW&speed=30&type=ssr', 'ssr.txt')
 
     savePoint(
-        'https://sspool.herokuapp.com/clash/proxies?nc=CN,HK,TW&speed=10,50&type=trojan', 'trojan.txt')
+        'https://sspool.herokuapp.com/clash/proxies?nc=CN,HK,TW&speed=30&type=trojan', 'trojan.txt')
 
 #    savePoint(
 #        'https://subsc.ednovas.xyz/sub?target=clash&url=https://raw.githubusercontent.com/ldir92664/Vmess-Actions/main/subscribe/vmess.txt&insert=false&config=https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini&emoji=true&list=false&tfo=false&scv=false&fdn=false&sort=false&expand=true&new_name=true', 'clash.yml')
@@ -40,7 +40,7 @@ def main(event, context):
         'https://subcon.dlj.tf/sub?target=clash&new_name=true&url=https%3A%2F%2Fraw.githubusercontent.com%2Fldir92664%2FVmess-Actions%2Fmain%2Fsubscribe%2Fvmess.txt%7Chttps%3A%2F%2Fraw.githubusercontent.com%2Fldir92664%2FVmess-Actions%2Fmain%2Fsubscribe%2Ftrojan.txt&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini', 'clash.yml')
 
     savePoint(
-        'https://suc.bihai.ml/sub?target=clash&new_name=true&url=https%3A%2F%2Fsspool.herokuapp.com%2Fclash%2Fproxies%3Fnc%3DCN%2CHK%2CTW%26speed%3D10%2C50%26type%3Dvmess%7Chttps%3A%2F%2Fsspool.herokuapp.com%2Fclash%2Fproxies%3Fnc%3DCN%2CHK%2CTW%26speed%3D10%2C50%26type%3Dtrojan&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini', 'clash-bihai.yml')
+        'https://suc.bihai.ml/sub?target=clash&new_name=true&url=https%3A%2F%2Fsspool.herokuapp.com%2Fclash%2Fproxies%3Fnc%3DCN%2CHK%2CTW%26speed%3D30%26type%3Dvmess%7Chttps%3A%2F%2Fsspool.herokuapp.com%2Fclash%2Fproxies%3Fnc%3DCN%2CHK%2CTW%26speed%3D30%26type%3Dtrojan&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini', 'clash-bihai.yml')
 
     
     # 获取文章地址
